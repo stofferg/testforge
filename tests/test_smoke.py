96cabd532c17db8d7707309c8fee5aaa06801e64
Gerrit Stoffer <stofferg@gmail.com>
2026-08-28 14:44:21 -0400

    Use Playwright assertions

--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -1,3 +1,14 @@
+import re
+from playwright.sync_api import expect
+
+
 def test_example(page):
     page.goto("https://example.com")
-    assert "Example Domain" in page.title()
+
+    link = page.get_by_role("link", name="Learn more")
+
+    expect(link).to_be_visible()
+
+    link.click()
+
+    expect(page).to_have_url("https://www.iana.org/help/example-domains")
